3162cc29da8847945e09f92dc894e9f03efdf75d
Kim Williame Lee <82743748+kimileeee@users.noreply.github.com>
2024-10-18 19:01:04 +0800

    Finished aavf_filter.py

--- workflow/scripts/aavf_filter.py
+++ workflow/scripts/aavf_filter.py
@@ -1,18 +1,21 @@
 import argparse
 import datetime
+import pandas as pd
+from io import StringIO
 
-def copy_headers(aavf_file, out_file):
+def copy_metadata(aavf_file, out_file):
     out_file.write("##fileformat=AAVFv1.0\n")
     out_file.write(f"##fileDate={datetime.datetime.now().strftime('%Y%m%d')}\n")
     out_file.write("##source=aavf_filter.py\n")
 
     for line in aavf_file:
-        if line.startswith("##reference") or line.startswith("##INFO") or line.startswith("##FILTER"):
-            out_file.write(line)
+        if line.startswith("##"):
+            if line.startswith("##reference") or line.startswith("##INFO") or line.startswith("##FILTER"):
+                out_file.write(line)
         else:
             break
 
-def add_filter_header(aavf_file, filter_params, out_file):
+def add_filter_metadata(filter_params, out_file):
     out_file.write("##FILTER=<ID=PASS,Description=\"All filters passed\">\n")
     filter_ids = [
         "AF<" + filter_params["max_AF"] if filter_params["max_AF"] else None,
@@ -34,6 +37,42 @@ def add_filter_header(aavf_file, filter_params, out_file):
         out_file.write(f"##FILTER=<ID={filter_ids[3]},Description=\"Coverage greater than {filter_params['min_cov']}\">\n")
 
     return filter_ids
+
+def parse_aavf(aavf_file):
+    aavf_headers = ["CHROM", "GENE", "POS", "REF", "ALT", "FILTER", "ALT_FREQ", "COVERAGE", "INFO"]
+    df = pd.read_csv(aavf_file, sep="\t", names=aavf_headers, comment="#")
+
+    info_df = df['INFO'].str.split(';', expand=True)
+    info_df = info_df.apply(lambda x: x.str.split('='), axis=1)
+    info_df = info_df.apply(lambda x: x.apply(lambda y: y[1] if len(y) > 1 else None))
+    info_df.columns = ["INFO.RC", "INFO.AC", "INFO.ACC", "INFO.ACF"]
+    
+    aavf_df = pd.concat([df, info_df], axis=1)
+    aavf_df = aavf_df.drop(columns=["INFO"])
+
+    # print(aavf_df)
+    return aavf_df
+
+def filter_aavf(aavf_data, filter_params, filter_ids):
+    filtered_aavf = aavf_data.copy()
+
+    # print(filtered_aavf.shape)
+    if filter_params["max_AF"]:
+        filtered_aavf = filtered_aavf[filtered_aavf["ALT_FREQ"] < float(filter_params["max_AF"])]
+
+    if filter_params["min_AF"]:
+        filtered_aavf = filtered_aavf[filtered_aavf["ALT_FREQ"] > float(filter_params["min_AF"])]
+
+    if filter_params["max_cov"]:
+        filtered_aavf = filtered_aavf[filtered_aavf["COVERAGE"] < int(filter_params["max_cov"])]
+
+    if filter_params["min_cov"]:
+        filtered_aavf = filtered_aavf[filtered_aavf["COVERAGE"] > int(filter_params["min_cov"])]
+
+    filtered_aavf["FILTER"] = "PASS"
+    # print(filtered_aavf.shape)
+
+    return filtered_aavf   
 
 def main():
     parser = argparse.ArgumentParser()
@@ -64,13 +103,17 @@ def main():
     if args.c:
         filter_params["min_cov"] = args.c
 
-    
-
-    with open(args.aavf) as aavf_file, open(args.out, 'w') as out_file:
-        copy_headers(aavf_file, out_file)
-        filter_ids = add_filter_header(aavf_file, filter_params, out_file)
+    with open(args.aavf, "r") as aavf_file, open(args.out, 'w') as out_file:
+        copy_metadata(aavf_file, out_file)
+        filter_ids = add_filter_metadata(filter_params, out_file)
         
         out_file.write("#CHROM\tGENE\tPOS\tREF\tALT\tFILTER\tALT_FREQ\tCOVERAGE\tINFO\n")
+
+        aavf_data = parse_aavf(aavf_file)
+        filtered_aavf = filter_aavf(aavf_data, filter_params, filter_ids)
+
+        for index, row in filtered_aavf.iterrows():
+            out_file.write(f"{row['CHROM']}\t{row['GENE']}\t{row['POS']}\t{row['REF']}\t{row['ALT']}\t{row['FILTER']}\t{row['ALT_FREQ']}\t{row['COVERAGE']}\tRC={row['INFO.RC']};AC={row['INFO.AC']};ACC={row['INFO.ACC']};ACF={row['INFO.ACF']}\n")
 
 if __name__ == "__main__":
     main()
